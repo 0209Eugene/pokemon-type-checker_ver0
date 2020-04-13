14f0ec8a87bba9ce0790d7dccc5756c8c14a61c6
Yuto Miura <veda.ym0209@gmail.com>
2020-04-14 01:48:08 +0900

    create client

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PokemonList from './components/PokemonList';
+
+function App() {
+  return (
+    <div id="main">
+      <h1>Pokemon Type Checker</h1>
+      <PokemonList />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/PokemonList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class PokemonList extends Component {
+  render() {
+    return (
+      <div>
+        <ul id="pokemonList">
+          <li>Pokemon name</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default PokemonList;
